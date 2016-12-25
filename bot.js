d76a49d9bc438db960657d48be28f9e48d0f31cf
mepho <metepolat.a@gmail.com>
2016-12-25 21:14:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/weed$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage("Are you kidding me? Grow up!");
     this.res.end();
   } else {
     console.log("don't care");
