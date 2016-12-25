1c2c2e6712443c140e3ecad48d3a83b58ae6117e
mepho <metepolat.a@gmail.com>
2016-12-25 23:54:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ var textArray = [
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^.*(weed|smoke|roll up).*$/;
+      botRegex = /^.*(weed|Weed|Smoke|smoke|roll up).*$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
